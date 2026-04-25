3a5c9aeb96dc67c12aa02255259e65088e11be24
dudbilopr <64034812+dudbilopr@users.noreply.github.com>
2026-04-25 06:36:50 -0500

    Add modulo4.js for Electrostática module content

---
+++ Modulos JS/modulo4.js
@@ -0,0 +1,96 @@
+export const modulo4 = {
+    "titulo": "Módulo 4: Electrostática - Distribución de Cargas Continuas",
+    "lecciones": [
+        {
+            "id": "m4-l1",
+            "tipo": "multivideo",
+            "recurso": "h57Q1fCwZgA|A9DWg5FSTx0|ZmBfYn9APkg|A9DWg5FSTx0|1cpACIOuFFU|k2dXe2AvoYs|k2w8tZeF0ug|IA5-ydK4ni8|8irn1C12G2U|MjhyOf-6VKQ|YisPzZHHvV0|ZmBfYn9APkg|cbePOxHemJQ|0wZ8s6Byghg|uesmpfag3Og|vWWwq63cyY8|EBddV2b2gCk|G3jlxpinO5U|HbYTaVAqCFs|1g5lqbuTqVw|SBPnw31wdsI|vAZ8SakEDNI|rT58Zxw6Cm4|VnbKz5u_mq4|fPJX3pkC6bM|alkbEKOgSM4|mgOiEQSQzhw|c7Yi0SQA-0g|TAV-vusYOPA|RMwjSPNjVu0|MakxGRwCJNs|piNXE_x7rds|19blKvJDkTM|ULMaUTcxu4g|p44FT5sA5iY|4cHTPtFuyUk|U7gvwwI-l20|_26VjgvsIeU|Yg5N1jHeDIQ|S5NDkJgDEig|NfjEvhwZDN0|Et92ofdz6y4|DnycOMVMI7E|aqoAXHZQ8aU|X1LAQryjbRo|gqbwBX7ZVvs",
+            "titulo": "4. Teoría: Distribuciones de Carga Continua",
+            "descripcion": "Selecciona la parte en el panel derecho.",
+            "llmLink": "https://notebooklm.google.com/notebook/37622815-4b54-4808-b770-37464cb05719"
+        },
+        {
+            "id": "m4-l2",
+            "tipo": "multipresentacion",
+            "recurso": "./clase_4.html|./clase_4.html",
+            "titulo": "2. Diapositivas de Apoyo (2 Partes)",
+            "descripcion": "Material visual utilizado en la clase magistral."
+        },
+        {
+            "id": "m4-g1",
+            "tipo": "grupo",
+            "titulo": "3. Laboratorio Virtual (Simuladores)",
+            "sublecciones": [
+                {
+                    "id": "m4-l3-a",
+                    "tipo": "simulador",
+                    "recurso": "Simuladores/Varilla_Cargada.html",
+                    "titulo": "Distribuciones de Carga Continua",
+                    "descripcion": "Entorno virtual para interactuar con cargas puntuales."
+                },
+                {
+                    "id": "m4-l3-b",
+                    "tipo": "simulador",
+                    "recurso": "Simuladores/Arco_Cargado.html",
+                    "titulo": "Distribuciones de Carga Continua",
+                    "descripcion": "Entorno virtual para interactuar con cargas puntuales."
+                },
+                {
+                    "id": "m4-l3-c",
+                    "tipo": "simulador",
+                    "recurso": "Simuladores/Linea_Rectangulo_Paralelepipedo (1).html",
+                    "titulo": "Distribuciones de Carga Continua",
+                    "descripcion": "Entorno virtual para interactuar con cargas puntuales."
+                },
+                {
+                    "id": "m4-l3-d",
+                    "tipo": "simulador",
+                    "recurso": "Simuladores/Anillo_Disco_Cilindro (1).html",
+                    "titulo": "Distribuciones de Carga Continua",
+                    "descripcion": "Entorno virtual para interactuar con cargas puntuales."
+                }
+            ]
+        },
+        {
+            "id": "m4-l7",
+            "tipo": "ejercicio",
+            "recurso": "Talleres/Taller_4_Distribuciones_de_Carga_Continua.html",
+            "titulo": "5. Taller Práctico No. 4",
+            "descripcion": "Guía de ejercicios sobre distribuciones de carga continúa."
+        },
+        {
+            "id": "m4-q1",
+            "tipo": "quiz",
+            "recurso": "Examen/Cuestionario_4_Distribuciones_de_Carga_continua.html",
+            "titulo": "6. Evaluación del Módulo 4",
+            "descripcion": "Cuestionario de validación de conocimientos."
+        },
+        {
+            "id": "m4-e1",
+            "tipo": "enlaces",
+            "titulo": "7. Enlaces de Interés",
+            "descripcion": "Directorio de recursos externos y herramientas útiles para el módulo.",
+            "secciones": [
+                {
+                    "tituloSeccion": "Lecturas y fundamentos teóricos",
+                    "links": [
+                        { "url": "https://openstax.org/...", "titulo": "OpenStax: Cálculo de campos...", "descripcion": "Explicación detallada..." },
+                        { "url": "http://hyperphysics.phy-astr.gsu.edu/...", "titulo": "Hyperphysics: Distribuciones...", "descripcion": "Conceptos fundamentales..." },
+                        { "url": "http://www.sc.ehu.es/...", "titulo": "Universidad del País Vasco...", "descripcion": "Análisis del campo..." },
+                        { "url": "https://espanol.libretexts.org/...", "titulo": "LibreTexts: Distribuciones...", "descripcion": "Enfoque de resolución..." },
+                        { "url": "https://cienciadelux.wordpress.com/...", "titulo": "Ciencia de Lux: Campo...", "descripcion": "Explicación didáctica..." }
+                    ]
+                },
+                {
+                    "tituloSeccion": "Material complementario (PDF)",
+                    "links": [
+                        { "url": "https://www.cerasa.es/...", "titulo": "Apuntes: Distribuciones...", "descripcion": "Material complementario..." },
+                        { "url": "http://www.das.uchile.cl/...", "titulo": "Problemas resueltos...", "descripcion": "Colección de problemas..." },
+                        { "url": "https://www2.udec.cl/...", "titulo": "Ejercicios de electrostática...", "descripcion": "Problemas prácticos..." },
+                        { "url": "http://www0.unsl.edu.ar/...", "titulo": "Sears y Zemansky: Física...", "descripcion": "Libro de texto clásico..." }
+                    ]
+                }
+            ]
+        }
+    ]
+};
