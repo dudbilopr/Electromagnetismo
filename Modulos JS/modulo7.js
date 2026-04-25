32f25b9ccd41c7107fd0fc16eb6c2d220ff71a05
dudbilopr <64034812+dudbilopr@users.noreply.github.com>
2026-04-25 06:38:54 -0500

    Add modulo7.js with Circuitos CC content

---
+++ Modulos JS/modulo7.js
@@ -0,0 +1,93 @@
+export const modulo7 = {
+    "titulo": "Módulo 7: Circuitos CC",
+    "lecciones": [
+        {
+            "id": "m7-l1",
+            "tipo": "multivideo",
+            "recurso": "COGjyZI1PS8|A4qBPrfzZMk|LYmdJmTyFGQ|5XdSmm8VWKQ|38lwnRYEGEc|3aWFJdqM_Rw|jJSq8BFJYt4|z-fv_QDESSc|0iU4RWMHqOg|Sw6vtplA_68|enQ8QuUBYNE|6w51PrhXy04|b_nBNDkJUJE|g2YK35fR7yE|XHAfc4EbH1Q|a7jaxpYtZAQ|DPCcImv1Xko|SizL_9HP6nY|gJasVOhHbao|7ssuVrxocBQ|X27VqqJ6Dwo|-iIUb9Bma0k|dxOxY0et1rE|9HCXZruI0Lk|gJasVOhHbao|TnA2ejc-1es|RmbQiiVV_bc|9t65kr3UVFw|a3VThqncuZQ",
+            "titulo": "5. Teoría: Ley de Gauss",
+            "descripcion": "Selecciona la parte en el panel derecho.",
+            "llmLink": "https://notebooklm.google.com/notebook/37622815-4b54-4808-b770-37464cb05719"
+        },
+        {
+            "id": "m7-l2",
+            "tipo": "multipresentacion",
+            "recurso": "./clase_7.html|https://docs.google.com/presentation/d/e/ID/pub?start=true&loop=true&delayms=3000",
+            "titulo": "2. Diapositivas de Apoyo (2 Partes)",
+            "descripcion": "Material visual utilizado en la clase magistral."
+        },
+        {
+            "id": "m7-g1",
+            "tipo": "grupo",
+            "titulo": "3. Laboratorio Virtual (Simuladores)",
+            "sublecciones": [
+                {
+                    "id": "m7-l3-a",
+                    "tipo": "simulador",
+                    "recurso": "Simuladores/Tabla_Gauss.html",
+                    "titulo": "Ley de Gauss: Esferas",
+                    "descripcion": "Entorno virtual para interactuar con esferas y superficies gaussianas"
+                },
+                {
+                    "id": "m7-l3-b",
+                    "tipo": "simulador",
+                    "recurso": "https://phys-viz.github.io/Gauss/",
+                    "titulo": "Distribuciones de Carga Continua",
+                    "descripcion": "Entorno virtual para interactuar con cargas puntuales."
+                },
+                {
+                    "id": "m7-l3-c",
+                    "tipo": "simulador",
+                    "recurso": "Simuladores/Flujo_Electrico.html",
+                    "titulo": "Distribuciones de Carga Continua",
+                    "descripcion": "Entorno virtual para interactuar con cargas puntuales."
+                }
+            ]
+        },
+        {
+            "id": "m7-l7",
+            "tipo": "ejercicio",
+            "recurso": "Talleres/Taller_5_1_Flujo_Electrico.html",
+            "titulo": "5. Taller Práctico No. 5",
+            "descripcion": "Guía de ejercicios sobre flujo eléctrico."
+        },
+        {
+            "id": "m7-q1",
+            "tipo": "quiz",
+            "recurso": "Examen/Cuestionario_5_Flujo_Electrico_Ley_de_Gauss.html",
+            "titulo": "6. Evaluación del Módulo 5",
+            "descripcion": "Cuestionario de validación de conocimientos."
+        },
+        {
+            "id": "m7-e1",
+            "tipo": "enlaces",
+            "titulo": "7. Enlaces de Interés",
+            "descripcion": "Directorio de recursos externos y herramientas útiles para el módulo.",
+            "secciones": [
+                {
+                    "tituloSeccion": "Fundamentos teóricos",
+                    "links": [
+                        { "url": "http://cima.utalca.cl/...", "titulo": "Flujo eléctrico y Ley de Gauss (UTalca)", "descripcion": "Desarrollo teórico..." },
+                        { "url": "http://www.das.uchile.cl/...", "titulo": "Electromagnetismo (U. de Chile)", "descripcion": "Material completo..." }
+                    ]
+                },
+                {
+                    "tituloSeccion": "Ejercicios resueltos",
+                    "links": [
+                        { "url": "https://www.uajms.edu.bo/...", "titulo": "Ejercicios resueltos (UAJMS)", "descripcion": "Problemas desarrollados..." },
+                        { "url": "https://fisicartes.files.wordpress.com/...", "titulo": "Problemas propuestos y resueltos", "descripcion": "Colección de ejercicios..." },
+                        { "url": "https://www.studocu.com/...", "titulo": "Problemas tipo examen (Studocu)", "descripcion": "Ejercicios de evaluaciones..." },
+                        { "url": "http://www.unizar.es/...", "titulo": "Ejercicios resueltos (Unizar)", "descripcion": "Problemas clásicos..." }
+                    ]
+                },
+                {
+                    "tituloSeccion": "Problemas propuestos",
+                    "links": [
+                        { "url": "http://www.sc.ehu.es/...", "titulo": "Problemas Ley de Gauss (EHU)", "descripcion": "Listado de problemas..." },
+                        { "url": "https://www.u-cursos.cl/...", "titulo": "Guía de ejercicios (U-Cursos)", "descripcion": "Material de curso..." }
+                    ]
+                }
+            ]
+        }
+    ]
+};
