31aa49d178d1afab89b7b4c46ffa56cb8a8454b2
dudbilopr <64034812+dudbilopr@users.noreply.github.com>
2026-04-25 06:44:27 -0500

    Add curriculo.js to export curriculum modules

---
+++ Modulos JS/curriculo.js
@@ -0,0 +1,19 @@
+// data/curriculo.js
+
+import { modulo1 } from './modulo1.js';
+import { modulo2 } from './modulo2.js';
+import { modulo3 } from './modulo3.js';
+import { modulo4 } from './modulo4.js';
+import { modulo5 } from './modulo5.js';
+import { modulo7 } from './modulo7.js';
+
+export const curriculoData = {
+    "modulos": [
+        modulo1,
+        modulo2,
+        modulo3,
+        modulo4,
+        modulo5,
+        modulo7
+    ]
+};
